911ca09df6959b4e37721acd03391379f879356b
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-08-21 20:54:00 +0530

    restarting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppU from './unicafe/AppU';
-import ComplexUseState from './components/ComplexUseState';
-import Anecdotes from './unicafe/anecdotes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Anecdotes />
-  </React.StrictMode>
+  <div>Hello World</div>
 );
